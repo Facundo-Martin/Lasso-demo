c023f1791010b428a5627e8a788d6f32a1b46ffc
Facundo-Martin <facundomartin98@gmail.com>
2022-12-08 11:31:58 -0800

    added SignIn providers

---
+++ pages/api/[...nextauth].js
@@ -0,0 +1,25 @@
+import NextAuth from "next-auth";
+import FacebookProvider from "next-auth/providers/facebook";
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    }),
+    // ...add more providers here
+  ],
+  secret: process.env.JWT_SECRET,
+};
+export default NextAuth(authOptions);
