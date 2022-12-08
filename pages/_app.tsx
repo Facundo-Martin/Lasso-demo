dac93962ee923f94e74953d4c9a823f22b4ed2d4
Facundo-Martin <facundomartin98@gmail.com>
2022-12-08 09:32:48 -0800

    app.tsx fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
-import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
-import { type AppType } from "next/app";
+
 import Layout from "../components/Layout/Layout";
 import "../styles/globals.css";
+
+import { type Session } from "next-auth";
+import { type AppType } from "next/app";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -23,17 +25,3 @@ const MyApp: AppType<{ session: Session | null }> = ({
 };
 
 export default MyApp;
-
-// export default function App({ Component, pageProps }: AppProps) {
-//   return (
-//     <>
-//       <SessionProvider session={pageProps.session}>
-//         <ThemeProvider enableSystem={true} attribute="class">
-//           <Layout>
-//             <Component {...pageProps} />
-//           </Layout>
-//         </ThemeProvider>
-//       </SessionProvider>
-//     </>
-//   );
-// }
